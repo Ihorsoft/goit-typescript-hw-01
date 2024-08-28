b67dd5538219194574bcb94ab9e5a814457d9852
Ihorsoft <0639492002s@gmail.com>
2024-08-28 20:37:21 +0300

    check 20-37

--- src/basic/7.ts
+++ src/basic/7.ts
@@ -3,7 +3,10 @@ interface IPage1Page2 {
   likes: number;
   accounts: string[];
   status: string;
-  details: {};
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
 }
 
 const page1: IPage1Page2 = {
